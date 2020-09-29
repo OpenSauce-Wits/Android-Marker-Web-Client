601a59c2ccdc71c2d3a5ddca581486a73a87dfcd
RamphoreTM <1908649@students.wits.ac.za>
2020-09-29 07:59:19 +0200

    Added buttons to control start and stop of marking

--- js/CreateTable.js
+++ js/CreateTable.js
@@ -1,3 +1,23 @@
+function handleClick(state){
+  if (state == 1){
+    if (this.innerHTML == "Marking"){
+      this.innerHTML = "Unmarked";
+      //CODE TO AFFECT DATABASE ADDED HERE
+    }else{
+      this.innerHTML = "Marking";
+      //CODE TO AFFECT DATABASE ADDED HERE
+    }
+  }else{
+    if (this.innerHTML == "Marking"){
+      this.innerHTML = "Free";
+      //CODE TO AFFECT DATABASE ADDED HERE
+    }else{
+      this.innerHTML = "Marking";
+      //CODE TO AFFECT DATABASE ADDED HERE
+    }
+  }
+}
+
 function CreateTableFromJSON(state) {
     /*For getting data from server
         var xmlhttp = new XMLHttpRequest();
@@ -11,97 +31,104 @@ function CreateTableFromJSON(state) {
         xmlhttp.send(); 
     */
 
-   /*dummy procedure of getting data from local file*/
+   /*DUMMY PROCEDURE OF GETTING DATA FROM LOCAL FILE*/
     // var myData = JSON.parse(Assignments);
 
-    var emulatorValues = [{"emulatorId":1,"status":"free","assignmentID":"N/a"},{"emulatorId":2,"status":"marking","assignmentID":8},{"emulatorId":3,"status":"marking","assignmentID":4},{"emulatorId":4,"status":"free","assignmentID":"N/a"},{"emulatorId":5,"status":"marking","assignmentID":3},{"emulatorId":6,"status":"free","assignmentID":"N/a"}];
+     var emulatorValues = [{"emulatorId":1,"status":"0","assignmentID":"N/a"},{"emulatorId":2,"status":"1","assignmentID":8},{"emulatorId":3,"status":"1","assignmentID":4},{"emulatorId":4,"status":"0","assignmentID":"N/a"},{"emulatorId":5,"status":"1","assignmentID":3},{"emulatorId":6,"status":"0","assignmentID":"N/a"}];
 
     var assignmentValues = [
         {
           "assignmentID": 1,
           "assignmentName": "Lab 3",
-          "status": "unmarked",
+          "status": "0",
           "priorityClass": 5,
           "assessor": "Dr Ranchod"
         },
         {
           "assignmentID": 2,
           "assignmentName": "Lab 2",
-          "status": "unmarked",
+          "status": "0",
           "priorityClass": 5,
           "assessor": "Dr Ranchod"
         },
         {
           "assignmentID": 3,
           "assignmentName": "Lab 3",
-          "status": "marking",
+          "status": "1",
           "priorityClass": 3,
           "assessor": "Dr Klein"
         },
         {
           "assignmentID": 4,
           "assignmentName": "Lab 2",
-          "status": "marking",
+          "status": "1",
           "priorityClass": 5,
           "assessor": "Dr Ranchod"
         },
         {
           "assignmentID": 5,
           "assignmentName": "Test 2",
-          "status": "unmarked",
+          "status": "0",
           "priorityClass": 3,
           "assessor": "Dr Klein"
         },
         {
           "assignmentID": 6,
           "assignmentName": "Lab 3",
-          "status": "unmarked",
+          "status": "0",
           "priorityClass": 5,
           "assessor": "Dr Ranchod"
         },
         {
           "assignmentID": 7,
           "assignmentName": "Test 1",
-          "status": "unmarked",
+          "status": "0",
           "priorityClass": 3,
           "assessor": "Dr Ranchod"
         },
         {
           "assignmentID": 8,
           "assignmentName": "Test 1",
-          "status": "marking",
+          "status": "1",
           "priorityClass": 1,
           "assessor": "Dr Ranchod"
         },
         {
           "assignmentID": 9,
           "assignmentName": "Lab 2",
-          "status": "unmarked",
+          "status": "0",
           "priorityClass": 5,
           "assessor": "Dr Klein"
         },
         {
           "assignmentID": 10,
           "assignmentName": "Lab 3",
-          "status": "unmarked",
+          "status": "0",
           "priorityClass": 5,
           "assessor": "Dr Ranchod"
         },
         {
           "assignmentID": 11,
           "assignmentName": "Lab 3",
-          "status": "unmarked",
+          "status": "0",
           "priorityClass": 5,
           "assessor": "Dr Ranchod"
         }
       ]
       ;
 
+      //BASED ON WHICH PAGE IS ACCESSED THE STATE DECIDES WHICH JSON TABLE TO RETRIEVE AND READ
     if(state == 1){
-       var values = assignmentValues;
+      //var values = require('AssignmentTable.json');
+      var values = assignmentValues;
     }else{
+      //  var request = new XMLHttpRequest();
+      //  request.open("GET","EmulatorTable.json",false);
+      //  request.send(null);
+      //  var values = JSON.parse(request.responseText);
        var values = emulatorValues;
     }
+
     // EXTRACT VALUE FOR HTML HEADER. 
     var col = [];
     for (var i = 0; i < values.length; i++) {
@@ -118,10 +145,14 @@ function CreateTableFromJSON(state) {
     // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
 
     var tr = table.insertRow(-1);                   // TABLE ROW.
+    var stat = -1;                                  // STATUS COLUMN VALUE
 
     for (var i = 0; i < col.length; i++) {
         var th = document.createElement("th");      // TABLE HEADER.
         th.innerHTML = col[i];
+        if (th.innerHTML == "status"){
+          stat = i;
+        }
         tr.appendChild(th);
     }
 
@@ -132,7 +163,23 @@ function CreateTableFromJSON(state) {
 
         for (var j = 0; j < col.length; j++) {
             var tabCell = tr.insertCell(-1);
-            tabCell.innerHTML = values[i][col[j]];
+            if (j != stat){
+              tabCell.innerHTML = values[i][col[j]];
+            }else{
+              if (state == 1){ //STATE IS MATTER OF ASSIGNMENT PAGE AGAINST EMULATOR PAGE
+                if (values[i][col[j]] ==  0){ //CHECKS STATUS OF SPECIFIC ENTRY
+                   tabCell.innerHTML = '<button class = "button" onclick="handleClick.call(this,1);">Unmarked</>';    //State value being passed as raw value as variable is considered undeclared
+                }else{
+                  tabCell.innerHTML = '<button class = "button" onclick="handleClick.call(this,1);">Marking</>';
+                }
+              }else{
+                if (values[i][col[j]] ==  0){
+                  tabCell.innerHTML = '<button class = "button" onclick="handleClick.call(this,2);">Free</>';
+               }else{
+                 tabCell.innerHTML = '<button class = "button" onclick="handleClick.call(this,2);">Marking</>';
+               }
+              }
+            }
         }
     }
 
